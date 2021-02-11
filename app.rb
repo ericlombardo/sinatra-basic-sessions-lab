f638b40b1bdb63f0eace910ae3ad8e7f7184b96f
Eric <eric.m.lombardo@gmail.com>
2021-02-11 16:24:15 -0500

    commit tree 07adc8f4c1602f8741a96560855c733a63c1e120

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
+require 'sysrandom/securerandom'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session['item'] = params[:item]
+    @session = session
+
+
+    erb :show_item
+  end
 end
