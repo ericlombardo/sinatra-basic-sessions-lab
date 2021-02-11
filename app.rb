cc334b698f420076d1c07cbccfd3881c995a75ad
Eric <eric.m.lombardo@gmail.com>
2021-02-11 16:24:54 -0500

    Build session hash using user input
    Enable sessions, create session_secret, build routes and views for
    submition, build 'POST' route, add to session hash, and display
    session from .erb file.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
+require 'sysrandom/securerandom'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    # sets session_secret to environment variable, if not available it creates it's own
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session['item'] = params[:item]
+    @session = session
+
+
+    erb :show_item
+  end
 end
